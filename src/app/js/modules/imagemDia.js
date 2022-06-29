797207777040ef0a4310547ac53139d394ff0005
Luciana Santos <luciydss@hotmail.com>
2022-06-29 14:13:18 -0300

    atualização na feature imagem do dia
    titulo adicionado; estilo do titulo; alteração na posição do link no menu

--- src/app/js/modules/imagemDia.js
+++ src/app/js/modules/imagemDia.js
@@ -1,11 +1,14 @@
 export default function fetchImagemDia(url, target) {
   const imagem = document.querySelector(target);
   const imgF0F = '/dist/assets/img/404.jpg';
+  const titulo = document.querySelector('[data-imagem="titulo"]');
+
   if (imagem) {
     fetch(url)
       .then((res) => res.json())
       .then((img) => {
         imagem.src = img.url;
+        titulo.innerText = img.title;
       })
       .catch((error) => {
         imagem.src = imgF0F;
